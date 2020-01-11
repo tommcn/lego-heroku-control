c73e6ae692d29b712e158cbc773b767e89163dfd
tommcn <tommcn@mcnamer.ca>
2020-01-11 18:06:07 -0500

    prepare for Rpi

--- app.py
+++ app.py
@@ -3,14 +3,20 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
-
-
 if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True)
